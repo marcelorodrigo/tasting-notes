87d19a10ae559dbe5f89a4a788d5f3b46adaa726
Marcelo Wiebbelling <mrodrigow@gmail.com>
2025-01-28 17:43:46 +0100

    Adjusting coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -11,6 +11,7 @@ export default mergeConfig(
       root: fileURLToPath(new URL('./', import.meta.url)),
       coverage: {
         provider: 'v8',
+        reporter: ['text', 'html', 'lcov', 'json'],
       },
     },
   }),
